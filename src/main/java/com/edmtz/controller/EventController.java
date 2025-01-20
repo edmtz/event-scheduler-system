33ec360ac4f37fbef500e96e3f30d8e54789886c
Eduardo González <eduardo_gonzm@hotmail.com>
2025-01-20 12:46:00 -0600

    config: config without Spring. Using Servlets only.

--- src/main/java/com/edmtz/controller/EventController.java
+++ src/main/java/com/edmtz/controller/EventController.java
@@ -2,6 +2,8 @@ package com.edmtz.controller;
 
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
 //@RestController
@@ -23,21 +26,39 @@ import java.io.PrintWriter;
 //
 //}
 
-public class EventController implements Controller {
+//public class EventController implements Controller {
+//
+//    @Override
+//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        // Configurar el tipo de contenido de la respuesta
+//        response.setContentType("text/plain");
+//        response.setCharacterEncoding("UTF-8");
+//
+//        // Escribir directamente en el cuerpo de la respuesta
+//        PrintWriter writer = response.getWriter();
+//        writer.write("Hola Mundo desde Spring 1.x");
+//        writer.flush();
+//
+//        // Retornar null porque no se usa una vista
+//        return null;
+//    }
+//
+//}
+
+public class EventController extends HttpServlet {
 
     @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // Configurar el tipo de contenido de la respuesta
-        response.setContentType("text/plain");
-        response.setCharacterEncoding("UTF-8");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
 
-        // Escribir directamente en el cuerpo de la respuesta
-        PrintWriter writer = response.getWriter();
-        writer.write("Hola Mundo desde Spring 1.x");
-        writer.flush();
+        String message = "Hello from Servlet!";
+        response.getWriter().write("<h1>" + message + "</h1>");
+    }
 
-        // Retornar null porque no se usa una vista
-        return null;
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String param = request.getParameter("param");
+        response.getWriter().write("<h1>Received POST param" + param + "</h1>");
     }
 
 }
